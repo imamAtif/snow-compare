b697ed6b198a6f580ab3b0e569519d4fd295b776
Atif Imam <mohd.atifimam@hotmail.com>
2026-07-02 15:26:21 +0530

    chore: add Python gitignore

--- snow_compare/cli.py
+++ snow_compare/cli.py
@@ -1,6 +1,6 @@
 import typer
 from rich.console import Console
-from snow_compare.sql_builder import build_row_count_sql
+from snow_compare.sql_builder import build_row_count_sql, build_distinct_count_sql
 
 app = typer.Typer(help="Snowflake Table row count Comparison SQL Generator")
 
@@ -22,5 +22,19 @@ def row_count(
     generate SQL query to compare row counts of two tables.
     """
     sql_query = build_row_count_sql(left, right)
+    console = Console()
+    console.print(sql_query, style="bold green")
+
+
+@app.command("distinct-count")
+def distinct_count(
+    left: str = typer.Option(..., "--left", "-l", help="Left table name"),
+    right: str = typer.Option(..., "--right", "-r", help="Right table name"),
+    key: str = typer.Option(..., "--key", "-k", help="Key column name"),
+) -> None:
+    """
+    generate SQL query to compare distinct key counts of two tables.
+    """
+    sql_query = build_distinct_count_sql(left, right, key)
     console = Console()
     console.print(sql_query, style="bold green")
--- snow_compare/sql_builder.py
+++ snow_compare/sql_builder.py
@@ -2,8 +2,21 @@ def build_row_count_sql(left_table: str, right_table: str) -> str:
     """
     Build SQL query to count rows in two tables."""
     sql_query = (
-        f"SELECT COUNT(*) as row_count FROM {left_table} "
-        f"UNION ALL SELECT COUNT(*) as row_count FROM {right_table};"
+        f"SELECT '{left_table}' AS table_name, COUNT(*) as row_count FROM {left_table} "
+        f"UNION ALL SELECT '{right_table}' AS table_name, COUNT(*) as row_count FROM {right_table};"
+    )
+
+    return sql_query
+
+
+def build_distinct_count_sql(left_table: str, right_table: str, key: str) -> str:
+    """
+    Build SQL query to compare distinct key counts in two tables.
+    """
+    sql_query = (
+        f"SELECT '{left_table}' AS table_name, COUNT(DISTINCT {key}) AS distinct_key_count FROM {left_table}\n"
+        "UNION ALL\n"
+        f"SELECT '{right_table}' AS table_name, COUNT(DISTINCT {key}) AS distinct_key_count FROM {right_table};"
     )
 
     return sql_query
