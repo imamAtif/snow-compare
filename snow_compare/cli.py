9c69125d91a673799f62731e1098447dc99f781a
Atif Imam <154099432+imamAtif@users.noreply.github.com>
2026-07-02 15:37:39 +0530

    Featrure row count comp (#1)
    * chore: add Python gitignore
    
    * feat: add SQL generation for missing keys in Snowflake tables

--- snow_compare/cli.py
+++ snow_compare/cli.py
@@ -1,6 +1,12 @@
 import typer
 from rich.console import Console
-from snow_compare.sql_builder import build_row_count_sql
+from snow_compare.sql_builder import (
+    build_missing_keys_both_tables_sql,
+    build_missing_keys_left_table_sql,
+    build_missing_keys_right_table_sql,
+    build_row_count_sql,
+    build_distinct_count_sql,
+)
 
 app = typer.Typer(help="Snowflake Table row count Comparison SQL Generator")
 
@@ -22,5 +28,61 @@ def row_count(
     generate SQL query to compare row counts of two tables.
     """
     sql_query = build_row_count_sql(left, right)
+    console = Console()
+    console.print(sql_query, style="bold green")
+
+
+@app.command("distinct-count")
+def distinct_count(
+    left: str = typer.Option(..., "--left", "-l", help="Left table name"),
+    right: str = typer.Option(..., "--right", "-r", help="Right table name"),
+    key: str = typer.Option(..., "--key", "-k", help="Key column name"),
+) -> None:
+    """
+    generate SQL query to compare distinct key counts of two tables.
+    """
+    sql_query = build_distinct_count_sql(left, right, key)
+    console = Console()
+    console.print(sql_query, style="bold green")
+
+
+@app.command("missing-keys-right")
+def missing_keys_right(
+    left: str = typer.Option(..., "--left", "-l", help="Left table name"),
+    right: str = typer.Option(..., "--right", "-r", help="Right table name"),
+    key: str = typer.Option(..., "--key", "-k", help="Key column name"),
+) -> None:
+    """
+    generate SQL query to find missing keys in the right table compared to the left table.
+    """
+    sql_query = build_missing_keys_right_table_sql(left, right, key)
+    console = Console()
+    console.print(sql_query, style="bold green")
+
+
+@app.command("missing-keys-left")
+def missing_keys_left(
+    left: str = typer.Option(..., "--left", "-l", help="Left table name"),
+    right: str = typer.Option(..., "--right", "-r", help="Right table name"),
+    key: str = typer.Option(..., "--key", "-k", help="Key column name"),
+) -> None:
+    """
+    generate SQL query to find missing keys in the left table compared to the right table.
+    """
+    sql_query = build_missing_keys_left_table_sql(left, right, key)
+    console = Console()
+    console.print(sql_query, style="bold green")
+
+
+@app.command("missing-keys-both")
+def missing_keys_both(
+    left: str = typer.Option(..., "--left", "-l", help="Left table name"),
+    right: str = typer.Option(..., "--right", "-r", help="Right table name"),
+    key: str = typer.Option(..., "--key", "-k", help="Key column name"),
+) -> None:
+    """
+    generate SQL query to find missing keys in both tables.
+    """
+    sql_query = build_missing_keys_both_tables_sql(left, right, key)
     console = Console()
     console.print(sql_query, style="bold green")
--- snow_compare/sql_builder.py
+++ snow_compare/sql_builder.py
@@ -2,8 +2,83 @@ def build_row_count_sql(left_table: str, right_table: str) -> str:
     """
     Build SQL query to count rows in two tables."""
     sql_query = (
-        f"SELECT COUNT(*) as row_count FROM {left_table} "
-        f"UNION ALL SELECT COUNT(*) as row_count FROM {right_table};"
+        f"SELECT '{left_table}' AS table_name, COUNT(*) as row_count FROM {left_table} "
+        f"UNION ALL SELECT '{right_table}' AS table_name, COUNT(*) as row_count FROM {right_table};"
     )
 
     return sql_query
+
+
+def build_distinct_count_sql(left_table: str, right_table: str, key: str) -> str:
+    """
+    Build SQL query to compare distinct key counts in two tables.
+    """
+    sql_query = (
+        f"SELECT '{left_table}' AS table_name, COUNT(DISTINCT {key}) AS distinct_key_count FROM {left_table}\n"
+        "UNION ALL\n"
+        f"SELECT '{right_table}' AS table_name, COUNT(DISTINCT {key}) AS distinct_key_count FROM {right_table};"
+    )
+
+    return sql_query
+
+
+def build_missing_keys_right_table_sql(
+    left_table: str, right_table: str, key: str
+) -> str:
+    """
+    Build SQL query to find missing keys in the right table compared to the left table.
+    """
+    sql_query = (
+        f"SELECT\n"
+        f"    l.{key}\n"
+        f"FROM {left_table} AS l\n"
+        f"LEFT JOIN {right_table} AS r\n"
+        f"    ON l.{key} = r.{key}\n"
+        f"WHERE r.{key} IS NULL;"
+    )
+
+    return sql_query
+
+
+def build_missing_keys_left_table_sql(
+    left_table: str, right_table: str, key: str
+) -> str:
+    """
+    Build SQL query to find missing keys in the left table compared to the right table.
+    """
+    sql_query = (
+        f"SELECT\n"
+        f"    r.{key}\n"
+        f"FROM {right_table} AS r\n"
+        f"LEFT JOIN {left_table} AS l\n"
+        f"    ON r.{key} = l.{key}\n"
+        f"WHERE l.{key} IS NULL;"
+    )
+
+    return sql_query
+
+
+def build_missing_keys_both_tables_sql(
+    left_table: str, right_table: str, key: str
+) -> str:
+    """
+    Build SQL query to find missing keys in both tables.
+    """
+    sql_query = (
+        f"SELECT\n"
+        f"    l.{key} AS missing_in_right\n"
+        f"FROM {left_table} AS l\n"
+        f"LEFT JOIN {right_table} AS r\n"
+        f"    ON l.{key} = r.{key}\n"
+        f"WHERE r.{key} IS NULL\n"
+        f"UNION ALL\n"
+        f"SELECT\n"
+        f"    r.{key} AS missing_in_left\n"
+        f"FROM {right_table} AS r\n"
+        f"LEFT JOIN {left_table} AS l\n"
+        f"    ON r.{key} = l.{key}\n"
+        f"WHERE l.{key} IS NULL;"
+    )
+
+    return sql_query
+
